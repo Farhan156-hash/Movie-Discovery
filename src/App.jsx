e7a2b8667bf80cc215b4b192f1ee1694c12b061e
Farhan156-hash <farhanofficial150@gmail.com>
2026-07-21 21:16:02 +0530

    Created MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import MovieCard from './components/MovieCard'
 
 function App() {
 
   return (
     <>
+    <MovieCard movie={{
+      title: "movie1",
+      release_date: "2025",
+    }}/>
 
+    <MovieCard movie={{
+      title: "movie2",
+      release_date: "2020",
+    }}/>
     </>
   )
 }
