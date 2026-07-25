d8f4e9bafe95f9409b56a2d38b9ba4ff21477fe9
Farhan156-hash <farhanofficial150@gmail.com>
2026-07-25 16:18:07 +0530

    Navbar for the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
 import Favorites from './pages/Favorites'
+import NavBar from './components/Navbar'
 
 function App() {
 
   return (
 
-    <main className="main-content">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/favorites' element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+
+      <main className="main-content">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
